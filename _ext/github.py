81c875459d57f51231266506b42f442901a0076a
Fabrizio Ferri Benedetti <fferribenedetti@splunk.com>
2023-02-14 10:36:04 +0100

    Format document

--- _ext/github.py
+++ _ext/github.py
@@ -1,7 +1,10 @@
-import ssl, sys, urllib
+import ssl
+import sys
+import urllib
 from docutils import nodes
 from docutils.parsers.rst import directives
 import sphinx.directives.code
+
 
 class GitHub(sphinx.directives.code.CodeBlock):
 
@@ -17,55 +20,56 @@ class GitHub(sphinx.directives.code.CodeBlock):
 
         url = self.options.get('url')
         if "raw.githubusercontent.com/signalfx" in url or "raw.githubusercontent.com/splunk" in url:
-          try:
-            call_context = ssl._create_unverified_context()
-            f = urllib.request.urlopen(url,context=call_context)
-            self.content = f.read().decode('utf-8').splitlines()
-          except urllib.error.HTTPError as e:
             try:
-              error = e.read().decode('utf-8')
-              message = '''This snippet couldn't be retrieved due to network issues.
+                call_context = ssl._create_unverified_context()
+                f = urllib.request.urlopen(url, context=call_context)
+                self.content = f.read().decode('utf-8').splitlines()
+            except urllib.error.HTTPError as e:
+                try:
+                    error = e.read().decode('utf-8')
+                    message = '''This snippet couldn't be retrieved due to network issues.
 
 Error: {error}
 
 To see the code, go to {url}
               '''.format(error=error, url=url)
-              self.content = message.splitlines()
-            except:
-              message = '''This snippet couldn't be retrieved due to HTTP errors.
+                    self.content = message.splitlines()
+                except:
+                    message = '''This snippet couldn't be retrieved due to HTTP errors.
 
 To see the code, go to {url}
               '''.format(url=url)
-              self.content = message.splitlines()
-          except urllib.error.URLError as e:
-            try:
-              error = e.read().decode('utf-8')
-              message = '''This snippet couldn't be retrieved due to URL errors.
+                    self.content = message.splitlines()
+            except urllib.error.URLError as e:
+                try:
+                    error = e.read().decode('utf-8')
+                    message = '''This snippet couldn't be retrieved due to URL errors.
 
 Error: {error}
 
 To see the code, go to {url}
               '''.format(error=error, url=url)
-              self.content = message.splitlines()
-            except:
-              message = '''This snippet couldn't be retrieved due to network issues.
+                    self.content = message.splitlines()
+                except:
+                    message = '''This snippet couldn't be retrieved due to network issues.
 
 To see the code, go to {url}
               '''.format(url=url)
-              self.content = message.splitlines()
-          except Exception as e:
-              error = e.read().decode('utf-8')
-              message = '''This snippet couldn't be downloaded due to network issues.
+                    self.content = message.splitlines()
+            except Exception as e:
+                error = e.read().decode('utf-8')
+                message = '''This snippet couldn't be downloaded due to network issues.
 
 Error: {error}
 
 To see the code, go to {url}
               '''.format(error=error, url=url)
-              self.content = message.splitlines()
+                self.content = message.splitlines()
         else:
             message = "Referenced file does not belong to Splunk repositories."
             self.content = message.splitlines()
         return super().run()
+
 
 def setup(app):
     app.add_directive("github", GitHub)
