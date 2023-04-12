cb5b03405c96c362ae7cf401a988924ed1a3d9dd
Fabrizio Ferri Benedetti <fferribenedetti@splunk.com>
2023-04-12 12:11:46 +0200

    Add defaults

--- _static/jsonpull-splunk.js
+++ _static/jsonpull-splunk.js
@@ -94,7 +94,12 @@ $(document).ready(function () {
                 'default': 'Category',
             };
 
-            let converter = new showdown.Converter();
+            let converter = new showdown.Converter(
+                {
+                    simplifiedAutoLink: true,
+                    excludeTrailingPunctuationFromURLs: true,
+                    literalMidWordUnderscores: true
+            });
             let cache = [];
             let idMap = [];
 
